8002ec9f3b5c2aa25f8aacbae8cf662127a7f5a7
Chahine-Chahine <chahinesquare@hotmail.com>
2023-12-02 17:11:17 +0200

    created server connection and backend structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("http://localhost/hospital-managment/src/Backend/Config/connection.php")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is your React App!
     </div>
   );
 }
---
+++ src/Backend/Config/connection.php
@@ -0,0 +1,20 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: *");
+
+
+$host = "localhost";
+$db_user = "root";
+$db_pass = null;
+$db_name = "hospitaldb";
+
+$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+
+if ($mysqli->connect_error) {
+    die("" . $mysqli->connect_error);
+} else {
+    echo "Connection successful";
+}
+?>
