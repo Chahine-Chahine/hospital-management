4f82812e8a7f764c4a8fb686b28d7517d9670cdd
Chahine-Chahine <chahinesquare@hotmail.com>
2023-12-06 00:56:39 +0200

    implemented modifications in the create doctor logic

--- Backend/API/Doctors/create.php
+++ Backend/API/Doctors/create.php
@@ -1,18 +1,76 @@
 <?php
+
 header('Access-Control-Allow-Origin:*');
 include("../../connection.php");
 
-
 $DoctorID = $_POST['DoctorID'];
-$UserID = $_POST['UserID'];
 
+$Username = $_POST['Username'];
+$RawPassword = $_POST['Password']; // Assuming you receive the raw, unhashed password
+$FirstName = $_POST['FirstName'];
+$LastName = $_POST['LastName'];
+$Email = $_POST['Email'];
+$Address = $_POST['Address'];
+$Specialization = $_POST['Specialization'];
+$RoleID = $_POST['RoleID']; // Assuming you have RoleID in your form data
 
-$query = $mysqli->prepare('insert into doctors(DoctorID, UserID) 
-values(?,?)');
-$query->bind_param('ii', $DoctorID , $UserID);
+// Hash the password
+$HashedPassword = password_hash($RawPassword, PASSWORD_DEFAULT);
+
+$insertUserQuery = $mysqli->prepare('INSERT INTO Users (RoleID, Username, Password, FirstName, LastName, Email, Address, Specialization) VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
+$insertUserQuery->bind_param('isssssss', $RoleID, $Username, $HashedPassword, $FirstName, $LastName, $Email, $Address, $Specialization);
+$insertUserQuery->execute();
+
+$UserID = $mysqli->insert_id;
+
+$insertDoctorQuery = $mysqli->prepare('INSERT INTO Doctors (DoctorID, UserID) VALUES (?, ?)');
+$insertDoctorQuery->bind_param('ii', $DoctorID, $UserID);
+$insertDoctorQuery->execute();
+
+$query = $mysqli->prepare('
+    SELECT
+        Users.UserID,
+        Users.Username,
+        Users.FirstName,
+        Users.LastName,
+        Users.Email,
+        Users.Address,
+        Users.Specialization
+    FROM
+        Users
+    INNER JOIN
+        Doctors ON Users.UserID = Doctors.UserID
+    WHERE
+        Doctors.DoctorID = ?
+');
+
+$query->bind_param('s', $DoctorID);
 $query->execute();
+$query->store_result();
+
+$query->bind_result($UserID, $Username, $FirstName, $LastName, $Email, $Address, $Specialization);
 
 $response = [];
-$response["status"] = "true";
+
+if ($query->num_rows > 0) {
+    $query->fetch();
+
+    $response['status'] = 'Success';
+    $response['user_id'] = $UserID;
+    $response['Username'] = $Username;
+    $response['Email'] = $Email;
+    $response['Address'] = $Address;
+    $response['Specialization'] = $Specialization;
+} else {
+    $response['status'] = 'Error';
+    $response['message'] = 'Doctor not found';
+}
 
 echo json_encode($response);
+
+$query->close();
+$insertUserQuery->close();
+$insertDoctorQuery->close();
+
+$mysqli->close();
+?>
--- src/Frontend/Pages/Admin/CreateDoctor.jsx
+++ src/Frontend/Pages/Admin/CreateDoctor.jsx
@@ -11,7 +11,9 @@ function Form() {
   const [email, setEmail] = useState("");
   const [address, setAddress] = useState("");
   const [specialization, setSpecialization] = useState("");
-  
+  const [doctorID, setDoctorID] = useState("");
+  const [deleteStatus, setDeleteStatus] = useState(null);
+
   const resetForm = () => {
     setRoleID("");
     setUsername("");
@@ -21,14 +23,17 @@ function Form() {
     setEmail("");
     setAddress("");
     setSpecialization("");
+    setDoctorID("");
+    setDeleteStatus(null);
   };
 
   const handleSignUp = async (e) => {
     e.preventDefault();
-
+  
     try {
-      const response = await sendRequest({
-        route: "Auth/signup",
+      // Create a new doctor
+      const createResponse = await sendRequest({
+        route: "Doctors/create",
         method: "POST",
         body: {
           RoleID: roleID,
@@ -39,22 +44,45 @@ function Form() {
           Email: email,
           Address: address,
           Specialization: specialization,
+          DoctorID: doctorID, 
         },
       });
-
-      console.log(response);
-
-     
-      if (response.status === "true") {
+  
+      console.log("Signup Response:", createResponse);
+  
+      if (createResponse && createResponse.status === "true") {
         console.log("User signed up successfully");
-        resetForm(); 
+  
+        // Retrieve the auto-generated UserID from the signup response
+        const userID = createResponse.user_id;
+  
+        // Insert a new record into the Doctors table
+        const insertDoctorResponse = await sendRequest({
+          route: "Doctors/create",
+          method: "POST",
+          body: {
+            DoctorID: doctorID,
+            UserID: userID,
+          },
+        });
+  
+        console.log("Insert Doctor Response:", insertDoctorResponse);
+  
+        if (insertDoctorResponse && insertDoctorResponse.status === "true") {
+          console.log("Doctor inserted successfully");
+        } else {
+          console.log("Failed to insert doctor. Status:", insertDoctorResponse ? insertDoctorResponse.status : "undefined");
+        }
+  
+        resetForm();
       } else {
-        console.log("Failed to sign up. Status:", response.status);
+        console.log("Failed to sign up. Status:", createResponse ? createResponse.status : "undefined");
       }
     } catch (error) {
-      console.error("Error signing up:", error.message);
+      console.error("Error signing up or inserting doctor:", error.message);
     }
   };
+  
 
   return (
     <div className="Container">
@@ -62,95 +90,106 @@ function Form() {
         <div className="wrapper">
           <h1>Create Doctor</h1>
           <form onSubmit={handleSignUp}>
-            <label htmlFor="roleID"></label>
+            <label htmlFor="roleID">Role ID:</label>
             <input
               type="text"
               id="roleID"
               name="RoleID"
-              placeholder="Role ID:"
+              placeholder="Role ID"
               value={roleID}
               onChange={(e) => setRoleID(e.target.value)}
               required
             />
 
-            <label htmlFor="username"></label>
+            <label htmlFor="username">Username:</label>
             <input
               type="text"
               id="username"
               name="Username"
-              placeholder="Username:"
+              placeholder="Username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
               required
             />
 
-            <label htmlFor="password"></label>
+            <label htmlFor="password">Password:</label>
             <input
               type="password"
               id="password"
               name="Password"
-              placeholder="Password:"
+              placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
 
-            <label htmlFor="firstName"></label>
+            <label htmlFor="firstName">First Name:</label>
             <input
               type="text"
               id="firstName"
               name="FirstName"
-              placeholder="First Name:"
+              placeholder="First Name"
               value={firstName}
               onChange={(e) => setFirstName(e.target.value)}
               required
             />
 
-            <label htmlFor="lastName"></label>
+            <label htmlFor="lastName">Last Name:</label>
             <input
               type="text"
               id="lastName"
               name="LastName"
-              placeholder="Last Name:"
+              placeholder="Last Name"
               value={lastName}
               onChange={(e) => setLastName(e.target.value)}
               required
             />
 
-            <label htmlFor="email"></label>
+            <label htmlFor="email">Email:</label>
             <input
               type="email"
               id="email"
               name="Email"
-              placeholder="Email:"
+              placeholder="Email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
             />
 
-            <label htmlFor="address"></label>
+            <label htmlFor="address">Address:</label>
             <input
               type="text"
               id="address"
               name="Address"
-              placeholder="Address:"
+              placeholder="Address"
               value={address}
               onChange={(e) => setAddress(e.target.value)}
               required
             />
 
-            <label htmlFor="specialization"></label>
+            <label htmlFor="specialization">Specialization:</label>
             <input
               type="text"
               id="specialization"
               name="Specialization"
-              placeholder="Specialization:"
+              placeholder="Specialization"
               value={specialization}
               onChange={(e) => setSpecialization(e.target.value)}
               required
             />
 
-            <button type="submit">Create</button>
+            <label htmlFor="doctorID">Doctor ID to insert:</label>
+            <input
+              type="number"
+              id="doctorID"
+              name="DoctorID"
+              placeholder="Doctor ID to insert"
+              value={doctorID}
+              onChange={(e) => setDoctorID(e.target.value)}
+              required
+            />
+
+            <button type="submit">Create Doctor</button>
           </form>
         </div>
       </div>
